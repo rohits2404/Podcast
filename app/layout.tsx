5fbf8cf269694e2af18f14dacdd1ff1da594d1b3
Rohit <rohits8731@gmail.com>
2026-09-07 16:08:12 +0530

    04: Convex

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -15,7 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
     return (
         <html lang="en">
-            <body className={manrope.className}>{children}</body>
+            <body className={manrope.className}>
+                <ConvexClientProvider>{children}</ConvexClientProvider>
+            </body>
         </html>
     );
 }
