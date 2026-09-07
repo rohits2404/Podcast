fec1e527ce043d3355057171da33199fe9968434
Rohit <rohits8731@gmail.com>
2026-09-07 17:46:14 +0530

    06: Create Podcast Page

---
+++ app/(root)/create-podcast/page.tsx
@@ -0,0 +1,8 @@
+import { CreatePodcastForm } from "@/features/create-podcast/form";
+import React from "react";
+
+const CreatePodcast = () => {
+    return <CreatePodcastForm />;
+};
+
+export default CreatePodcast;
