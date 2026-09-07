a4e1342e18269e383ebb317910ce472ea89dd3fc
Rohit <rohits8731@gmail.com>
2026-09-07 15:21:00 +0530

    01: Project Setup

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+
+const manrope = Manrope({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Voxora",
+    description: "Generate Your Podcasts Using AI",
+    icons: {
+        icon: "/icons/logo.png",
+    },
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+    return (
+        <html lang="en">
+            <body className={manrope.className}>{children}</body>
+        </html>
+    );
+}
